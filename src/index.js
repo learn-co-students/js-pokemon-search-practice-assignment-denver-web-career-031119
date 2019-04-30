100d2dd5f0d73f312bc7f5e79b2b989a8a370353
yeah-right <claar.zack@gmail.com>
2019-04-30 13:06:46 -0600

    cards are populating but there are now pictures there yet. The src is correct, trying to figure this out

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
+  // console.log(POKEMON)
   //YOUR CODE HERE
-})
+  const thePokemonContainer = document.querySelector('#pokemon-container');
+
+  thePokemonContainer.innerHTML = renderAllThePokemon(POKEMON);
+
+  thePokemonContainer.addEventListener('click', (event) => {
+    console.log("clicked");
+  })
+
+  function renderAllThePokemon(fullPokemonArray) {
+    return fullPokemonArray.map(renderSinglePokemon).join('');
+  }
+
+  function renderSinglePokemon(singlePokemon) {
+    console.log(singlePokemon.sprites.front);
+    // console.log("----------------------");
+    
+    return (`
+      </div class="pokemon-card">
+        <div class="pokemon-frame">
+          <h1 class="center-text">${singlePokemon.name}</h1>
+          <div class="pokemon-image>
+            <img data-id="${singlePokemon.id}" data-action="flip" class="toggle-sprite" src="${singlePokemon.sprites.front}" />  
+          </div
+        </div>
+      </div>
+    `)
+  };
+});
