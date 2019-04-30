7e50eb46dc8bf68e42af8abf3430c8700d803583
yeah-right <claar.zack@gmail.com>
2019-04-30 13:40:44 -0600

    pokemon being flipped front to back images

--- src/index.js
+++ src/index.js
@@ -5,9 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   thePokemonContainer.innerHTML = renderAllThePokemon(POKEMON);
 
-  // thePokemonContainer.addEventListener('click', (event) => {
-  //   console.log("clicked");
-  // })
+  thePokemonContainer.addEventListener('click', (event) => {
+    // console.log(event);
+    if (event.target.dataset.action === 'flip') {
+      // console.log('working');
+      // console.log(event);
+      
+      //now need to find the id of the pokemon that was clicked
+      //and the syntax is different than in ruby fml
+      const clickedPokemon = POKEMON.find(function(pokemon) {
+        return pokemon.id == event.target.dataset.id
+      })
+      console.log(clickedPokemon);
+      //need to check which image it is in order to change the sprites.front to sprites.back
+      if (event.target.src === clickedPokemon.sprites.front) {
+        console.log(event.target.src);
+        event.target.src = clickedPokemon.sprites.back;
+      } else {
+        event.target.src = clickedPokemon.sprites.front
+      }
+      
+      
+    }
+  })
 
   function renderAllThePokemon(fullPokemonArray) {
     return fullPokemonArray.map(renderSinglePokemon).join('');
